4ea174452aedd50b8a9a4f74fa3428d84b968d81
Zeysxla <104156860+Zeysxla@users.noreply.github.com>
2022-04-21 23:02:28 +0700

    Add files via upload

---
+++ If Else Condition.py
@@ -0,0 +1,18 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> hari_ini = "Minggu"
+
+if(hari_ini == "Senin"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Selasa"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Kamis"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Jumat"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Sabtu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Minggu"):
+    print("Saya akan libur")
